f570758842a93b76016ad01edd0e32c5f89c4278
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2023-09-24 22:03:00 -0300

    config .env, pm2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require("express-async-errors");
+require("dotenv/config");
+
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
@@ -31,5 +33,5 @@ app.use(( error, request, response, next ) => {
     })
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
